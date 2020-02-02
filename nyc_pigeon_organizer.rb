b31fdfa6352b6779681b7f03a717afe0c5443733
Roger Robertson Jr <rjrjr@pm.me>
2020-02-02 22:57:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,11 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeons = {}
   data[:gender].each { |gender|
-    puts "GENDER"
-    pp gender[1][1]
     gender.each { |m_or_f|
-    puts "M_OR_F"
-    pp m_or_f
+    index = 0
+    name = 0
+    while index < gender.length
+      while name < gender[index].length
     pigeons[gender[index][name]] = {
       :color => [],
       :gender => m_or_f.to_s,
