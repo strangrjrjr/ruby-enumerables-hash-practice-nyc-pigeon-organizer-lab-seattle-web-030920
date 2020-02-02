9f182ef41fc99982189b859ecfa44225cba50206
Roger Robertson Jr <rjrjr@pm.me>
2020-02-02 23:00:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   pigeons = {}
   data[:gender].each { |gender|
     gender.each { |m_or_f|
-    index = 0
+    index = 1
     name = 0
     while index < gender.length
       while name < gender[index].length
