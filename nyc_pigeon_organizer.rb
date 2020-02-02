173b0f8791b691e6e71d511e297d2bb096cbeb8b
Roger Robertson Jr <rjrjr@pm.me>
2020-02-02 22:49:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeons = {}
+  data[:gender].each { |gender|
+    puts "GENDER"
+    pp gender[1][1]
+    gender.each { |m_or_f|
+    puts "M_OR_F"
+    pp m_or_f
+      m_or_f.each { |bird|
+        pigeons[bird] = {
+          :color => [],
+          :gender => m_or_f.to_s,
+          :lives => ""
+        }
+      }
+    }
+  }
+  pigeons 
 end
